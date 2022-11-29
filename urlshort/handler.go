fb6dacd128217394a345fd45304e060ea7371c46
Daniel <dani2014games@gmail.com>
2022-11-29 12:31:27 -0300

    fix: Adicionado comentários no urlshort

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -1,55 +1,55 @@
 package urlshort  
 
-import (
+import ( //Importando pacotes
 	"net/http"
 
 	yaml "gopkg.in/yaml.v2"
 )
 
 
-func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		path := r.URL.Path
-		if dest, ok := pathsToUrls[path]; ok {
-			http.Redirect(w, r, dest, http.StatusFound)
-			return
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc { //Função que recebe um map que recebe uma string e retorna uma string e um handler e retorna um handler
+	return func(w http.ResponseWriter, r *http.Request) { //Retorna uma função que recebe um responseWriter e um request
+		path := r.URL.Path //Armazena o path do request na variável path
+		if dest, ok := pathsToUrls[path]; ok { //Verifica se o path existe no map
+			http.Redirect(w, r, dest, http.StatusFound) //Redireciona para o path
+			return //Retorna
 		}
-		fallback.ServeHTTP(w, r)
+		fallback.ServeHTTP(w, r) //Se não existir, chama o fallback
 	}
 }
 
-func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	
-	pathUrls, err := parseYaml(yamlBytes)
+func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) { //Função que recebe um slice de bytes e um handler e retorna um handler e um erro
+	  
+	pathUrls, err := parseYaml(yamlBytes) //Armazena o slice de bytes na variável pathUrls e verifica se tem erro
 	if err != nil {
 		return nil, err
 	}
 
 	
-	pathsToUrls := buildMap(pathUrls)
+	pathsToUrls := buildMap(pathUrls) //Armazena o map na variável pathsToUrls
 
 	
-	return MapHandler(pathsToUrls, fallback), nil
+	return MapHandler(pathsToUrls, fallback), nil //Retorna o mapHandler
 }
 
-func buildMap(pathUrls []pathUrl) map[string]string {
-	pathsToUrls := make(map[string]string)
-	for _, pu := range pathUrls {
-		pathsToUrls[pu.Path] = pu.URL
+func buildMap(pathUrls []pathUrl) map[string]string { //Função que recebe um slice de pathUrl e retorna um map que recebe uma string e retorna uma string
+	pathsToUrls := make(map[string]string) //Cria um map que recebe uma string e retorna uma string
+	for _, pu := range pathUrls { //Percorre o slice de pathUrl
+		pathsToUrls[pu.Path] = pu.URL //Adiciona o path e a url no map
 	}
-	return pathsToUrls
+	return pathsToUrls //Retorna o map
 }
 
-func parseYaml(data []byte) ([]pathUrl, error) {
-	var pathUrls []pathUrl
-	err := yaml.Unmarshal(data, &pathUrls)
+func parseYaml(data []byte) ([]pathUrl, error) { //Função que recebe um slice de bytes e retorna um slice de pathUrl e um erro caso tenha
+	var pathUrls []pathUrl //Cria um slice de pathUrl
+	err := yaml.Unmarshal(data, &pathUrls) //Deserializa o slice de bytes e armazena no slice de pathUrl e verifica se tem erro
 	if err != nil {
 		return nil, err
 	}
-	return pathUrls, nil
+	return pathUrls, nil //Retorna o slice de pathUrl e nil
 }
 
-type pathUrl struct {
-	Path string `yaml:"path"`
+type pathUrl struct { //Cria uma struct que recebe uma string e retorna uma string
+	Path string `yaml:"path"` 
 	URL  string `yaml:"url"`
 }
--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -1,45 +1,45 @@
 package main
 
-import (
+import (  //Importando pacotes
 	"fmt"
 	"net/http"
 
 	"urlshort"
 )
 
-func main() {
-	mux := defaultMux()
+func main() {  //Função principal
+	mux := defaultMux() //Armazenando o mux padrão na variável mux
 
 
-	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+	pathsToUrls := map[string]string{ //Criando um map que recebe uma string e retorna uma string
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort", 
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
-	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux) //Armazenando o mapHandler na variável mapHandler
 
 	
-	yaml := `
+	yaml := ` #Criando um slice de yaml
 - path: /urlshort
   url: https://github.com/gophercises/urlshort
 - path: /urlshort-final
   url: https://github.com/gophercises/urlshort/tree/solution
 `
 
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler) //Armazenando o yamlHandler na variável yamlHandler e verificando se tem erro
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Iniciando o servidor na porta :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	fmt.Println("Iniciando o servidor na porta :8080") //Imprime a mensagem
+	http.ListenAndServe(":8080", yamlHandler) //Inicia o servidor na porta 8080
+} 
+
+func defaultMux() *http.ServeMux { //Função que retorna um mux padrão
+	mux := http.NewServeMux() //Cria um mux
+	mux.HandleFunc("/", hello) //Adiciona a função hello no mux
+	return mux //Retorna o mux
 }
 
-func defaultMux() *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", hello)
-	return mux
-}
-
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Olá, mundo!")
+func hello(w http.ResponseWriter, r *http.Request) { //Função que imprime a mensagem
+	fmt.Fprintln(w, "Olá, mundo!") //Imprime a mensagem
 }
 	
