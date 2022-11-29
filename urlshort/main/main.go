fb970456393b3232a40fac5e792db63e645fee0c
DEEPLERZERA <dani2014games@gmail.com>
2022-11-29 20:36:24 -0300

    (:

--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -1,23 +1,21 @@
 package main
 
-import (  //Importando pacotes
+import ( //Importando pacotes
 	"fmt"
 	"net/http"
 
 	"urlshort"
 )
 
-func main() {  //Função principal
+func main() { //Função principal
 	mux := defaultMux() //Armazenando o mux padrão na variável mux
 
-
 	pathsToUrls := map[string]string{ //Criando um map que recebe uma string e retorna uma string
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort", 
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
-	mapHandler := urlshort.MapHandler(pathsToUrls, mux) //Armazenando o mapHandler na variável mapHandler
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux) //Armazenando o mapHandler na variável mapHandler (: aaa
 
-	
 	yaml := ` #Criando um slice de yaml
 - path: /urlshort
   url: https://github.com/gophercises/urlshort
@@ -30,16 +28,15 @@ func main() {  //Função principal
 		panic(err)
 	}
 	fmt.Println("Iniciando o servidor na porta :8080") //Imprime a mensagem
-	http.ListenAndServe(":8080", yamlHandler) //Inicia o servidor na porta 8080
-} 
+	http.ListenAndServe(":8080", yamlHandler)          //Inicia o servidor na porta 8080
+}
 
 func defaultMux() *http.ServeMux { //Função que retorna um mux padrão
-	mux := http.NewServeMux() //Cria um mux
+	mux := http.NewServeMux()  //Cria um mux
 	mux.HandleFunc("/", hello) //Adiciona a função hello no mux
-	return mux //Retorna o mux
+	return mux                 //Retorna o mux
 }
 
 func hello(w http.ResponseWriter, r *http.Request) { //Função que imprime a mensagem
 	fmt.Fprintln(w, "Olá, mundo!") //Imprime a mensagem
 }
-	
