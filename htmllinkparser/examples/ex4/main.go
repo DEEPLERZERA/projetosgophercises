69b767db6411bc8ea6d56616988f7aa808dbb1b7
Daniel <dani2014games@gmail.com>
2023-01-06 11:48:02 -0300

    Finishing htmllinkparser

---
+++ htmllinkparser/examples/ex4/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	link "htmllinkparser"
+
+	"strings"
+)
+
+var exampleHTML = `
+<html>
+<body>
+  <a href="/dog-cat">dog cat <!-- commented text SHOULD NOT be included! --></a>
+</body>
+</html>
+`
+
+func main() {
+	r := strings.NewReader(exampleHTML)
+	links, err := link.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", links)
+}
